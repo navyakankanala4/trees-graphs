b0927cb69202f8f4b1b071c5b974ef39894acc2f
navyakankanala4 <31644078+navyakankanala4@users.noreply.github.com>
2017-09-26 06:55:37 +0000

    Delete header.h

--- Desktop/trees-graphs/adjacency array/include/header.h
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define MAX 100
-int adj[MAX][MAX];
-int n;
-void create_graph();
-void display();
-void insert_edge(int source,int dest);
-void del_edge(int source,int dest);
